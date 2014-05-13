3cac1dc6013e3e175dd495aedfffcc09005daa4a
zhang zhenfang <fangzhzh@gmail.com>
2014-05-13 15:02:17 +0800

    chapter2 example

---
+++ 394.cpp
@@ -0,0 +1,26 @@
+/*
+ID: fangzhz1
+PROG: transform
+LANG: C++
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <
+using namespace std;
+int main(int argc, char ** argv)
+{
+    long Base;
+    vector<int> Dimentions;
+    vector<int> Ls;
+    vector<int> Us;
+    vector<int> Cs;
+
+    // read file
+    ifstream if()
+    // init file
+    // calc
+
+    return 0;
+}
